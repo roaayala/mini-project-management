14870e9bb898ab31500184a8fe5da0f2212e3ba6
roaayala <roaayala.ahm@gmail.com>
2026-05-07 21:43:31 +0800

    component: ItemDialog

--- src/components/main-panels/MainPanelContent.js
+++ src/components/main-panels/MainPanelContent.js
@@ -1,13 +1,29 @@
 import createEmptyMessage from "../commons/EmptyMessage.js";
+import createButton from "../commons/Button.js";
+import showItemDialog from "../dialogs/ItemDialog.js";
 
 export default function createMainPanelContent({ workspace }) {
 	const mainPanelContent = document.createElement("main");
 	mainPanelContent.className = "workspace-content";
 
+	const showButton = createButton({
+		id: "projectDialog",
+		style: "btn btn-primary",
+		type: "button",
+		text: "New Project",
+		callback: () => {
+			showItemDialog({ dialogTitle: "New Project Details" });
+		},
+	});
+
 	if (workspace.projects.items.length === 0) {
 		const emptyMessage = createEmptyMessage("No project being added!");
 		mainPanelContent.appendChild(emptyMessage);
+		mainPanelContent.appendChild(showButton);
+		return mainPanelContent;
 	}
+
+	mainPanelContent.appendChild(showButton);
 
 	return mainPanelContent;
 }

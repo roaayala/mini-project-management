504244f6d2831136bbdba1232bb65ea640f820c5
roaayala <roaayala.ahm@gmail.com>
2026-05-11 21:19:21 +0800

    component: SidebarHeader

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,7 +1,16 @@
+import createSidebarHeader from "./SidebarHeader.js";
+
 export default function createSidebar() {
 	const sidebar = document.createElement("aside");
 	sidebar.className = "sidebar";
-	sidebar.textContent = "sidebar";
+
+	// header
+	const sidebarHeader = createSidebarHeader();
+	sidebar.appendChild(sidebarHeader);
+
+	// content
+
+	// actions
 
 	return sidebar;
 }

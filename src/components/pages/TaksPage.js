93e9954311da5b0b54ec58e9cbd066df80b7d2fd
roaayala <roaayala.ahm@gmail.com>
2026-05-18 22:04:24 +0800

    component: TaskPage

--- src/components/pages/TaksPage.js
+++ src/components/pages/TaksPage.js
@@ -11,5 +11,30 @@ export default function createTaskPage({ task, todos, actions }) {
   const pageHeader = createPageHeader({ item: task });
   taskPage.appendChild(pageHeader);
 
+  // CONTENT
+  const taskTodos = todos.filter((todo) => todo.tsId === task.id);
+  const pageContent = document.createElement("main");
+  pageContent.className = "page-content";
+
+  if (taskTodos.length === 0) {
+    const emptyTodos = createEmptyMessage("No todo being added!");
+    pageContent.appendChild(emptyTodos);
+  } else {
+    console.log(taskTodos);
+  }
+
+  taskPage.appendChild(pageContent);
+
+  // ACTION
+  const pageAction = createPageAction({
+    buttonConfig: {
+      text: "New Task",
+      callback: () => {
+        console.log(task.id);
+      },
+    },
+  });
+  taskPage.appendChild(pageAction);
+
   return taskPage;
 }
